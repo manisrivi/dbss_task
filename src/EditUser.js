235b7c04fde4c62590bfcaf28df9d40179211c22
manisrivi <manisrivi1995@mail.com>
2022-11-11 13:44:13 +0530

    dbss_task

---
+++ src/EditUser.js
@@ -0,0 +1,236 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function EditUser() {
+  // user Validation schema
+  const UserSchema = Yup.object().shape({
+    name: Yup.string().required("Required"),
+    dob: Yup.string().required("Required"),
+    gender: Yup.string().required("Required"),
+    role: Yup.string().required("Required"),
+    skill: Yup.array("").required("Required"),
+    otherSkills: Yup.string(),
+    address: Yup.string().required("Required"),
+    comments: Yup.string().required("Required"),
+  });
+
+  // use params use to get from index
+  const { index } = useParams();
+
+  // navigate the page
+  const navigate = useNavigate("");
+
+  // get from user data form localStorage
+  const user_data_list = localStorage.getItem("user");
+
+  // convert the array
+  const user_list = JSON.parse(user_data_list);
+
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          // fetch the data
+          name: user_list[index].name,
+          dob: user_list[index].dob,
+          gender: user_list[index].gender,
+          role: user_list[index].role,
+          skill: user_list[index].skill,
+          otherSkills: user_list[index].otherSkills,
+          address: user_list[index].address,
+          comments: user_list[index].comments,
+        }}
+        validationSchema={UserSchema}
+        onSubmit={(values) => {
+          // change the data from localStorage
+          const users =
+            localStorage.getItem("user") &&
+            localStorage.getItem("user").length > 0
+              ? JSON.parse(localStorage.getItem("user"))
+              : [];
+          const _user = users.map((user, userInIndex) => {
+            if (userInIndex == localStorage.getItem("editIndex")) {
+              return values;
+            } else {
+              return user;
+            }
+          });
+          localStorage.setItem("user", JSON.stringify(_user));
+          navigate("/");
+        }}
+      >
+        {({ errors, touched }) => (
+          <div className="container mb-5 p-5">
+            <div className="row justify-content-center">
+              <Form className="col-5 d-flex flex-column gap-2 rounded-4 shadow-lg p-5">
+                <h4 className="text-primary text-center">EDIT USER</h4>
+                <div>
+                  <h6>
+                    Name <span className="text-danger">*</span>
+                  </h6>
+                  <Field name="name" className="form-control" />
+                  {errors.name && touched.name ? (
+                    <div className="text-danger">{errors.name}*</div>
+                  ) : null}
+                </div>
+                <div>
+                  <h6>
+                    D.O.B <span className="text-danger">*</span>
+                  </h6>
+                  <Field name="dob" type="date" className="form-control" />
+                  {errors.dob && touched.dob ? (
+                    <div className="text-danger">{errors.dob}*</div>
+                  ) : null}
+                </div>
+
+                <div>
+                  <h6>
+                    Gender <span className="text-danger">*</span>
+                  </h6>
+                  <div id="my-radio-group"></div>
+                  <div
+                    role="group"
+                    className="d-flex gap-3"
+                    aria-labelledby="my-radio-group"
+                  >
+                    <label>
+                      <Field type="radio" name="gender" value="male" />
+                      Male
+                    </label>
+                    <label>
+                      <Field type="radio" name="gender" value="female" />
+                      Female
+                    </label>
+                  </div>
+                  {errors.gender && touched.gender ? (
+                    <div className="text-danger">{errors.gender}*</div>
+                  ) : null}
+                </div>
+                <div>
+                  <h6>
+                    Role <span className="text-danger">*</span>
+                  </h6>
+                  <Field
+                    name="role"
+                    component="select"
+                    className="form-control text-center"
+                  >
+                    <option hidden selected>
+                      {" "}
+                      -- Select one --
+                    </option>
+                    <option value="Front end developer">
+                      Front end developer
+                    </option>
+                    <option value="Back end developer">
+                      Back end developer
+                    </option>
+                    <option value="Devops">Devops</option>
+                    <option value="Data Engineer">Data Engineer</option>
+                  </Field>
+                  {errors.role && touched.role ? (
+                    <div className="text-danger">{errors.role}*</div>
+                  ) : null}
+                </div>
+
+                <div>
+                  <h6>
+                    Skills <span className="text-danger">*</span>
+                  </h6>
+                  <div id="checkbox-group"></div>
+                  <div
+                    role="group"
+                    aria-labelledby="checkbox-group"
+                    className="d-flex flex-wrap gap-3"
+                  >
+                    <label>
+                      <Field type="checkbox" name="skill" value="React.Js" />
+                      React.Js
+                    </label>
+                    <label>
+                      <Field type="checkbox" name="skill" value="Angular" />
+                      Angular
+                    </label>
+                    <label>
+                      <Field type="checkbox" name="skill" value="Dot net" />
+                      Dot net
+                    </label>
+                    <label>
+                      <Field type="checkbox" name="skill" value="Java" />
+                      Java
+                    </label>
+                    <label>
+                      <Field type="checkbox" name="skill" value="Node.Js" />
+                      Node.Js
+                    </label>
+                    <label>
+                      <Field type="checkbox" name="skill" value="AWS" />
+                      Dot net
+                    </label>
+                    <label>
+                      <Field type="checkbox" name="skill" value="Azure" />
+                      Azure
+                    </label>
+                  </div>
+                  {errors.skill && touched.skill ? (
+                    <div className="text-danger">{errors.skill}*</div>
+                  ) : null}
+                </div>
+
+                <div>
+                  <h6>
+                    Other Skills <span className="text-danger"></span>
+                  </h6>
+                  <Field
+                    name="otherSkills"
+                    component="textarea"
+                    className="form-control"
+                  />
+                  {errors.otherSkills && touched.otherSkills ? (
+                    <div className="text-danger">{errors.otherSkills}*</div>
+                  ) : null}
+                </div>
+
+                <div>
+                  <h6>
+                    Address <span className="text-danger">*</span>
+                  </h6>
+                  <Field
+                    name="address"
+                    component="textarea"
+                    className="form-control"
+                  />
+                  {errors.address && touched.address ? (
+                    <div className="text-danger">{errors.address}*</div>
+                  ) : null}
+                </div>
+
+                <div>
+                  <h6>
+                    Comments <span className="text-danger">*</span>
+                  </h6>
+                  <Field
+                    name="comments"
+                    component="textarea"
+                    className="form-control"
+                  />
+                  {errors.comments && touched.comments ? (
+                    <div className="text-danger">{errors.comments}*</div>
+                  ) : null}
+                </div>
+                <button
+                  type="submit"
+                  className="form-control btn btn-outline-warning fw-bold"
+                >
+                  Update
+                </button>
+              </Form>
+            </div>
+          </div>
+        )}
+      </Formik>
+    </div>
+  );
+}
---
+++ src/Home.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { MdModeEdit } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
+
+export default function Home() {
+  // Navigate
+  const navigate = useNavigate("");
+
+  // get users
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () => {
+    const user = localStorage.getItem("user");
+    setUsers(JSON.parse(user));
+  };
+
+  // delete user
+  const handleDelete = (index) => {
+    const user_list = users.filter((ele, del_index) => {
+      if (index !== del_index) {
+        return ele;
+      }
+    });
+    setUsers(user_list);
+    localStorage.setItem("user", JSON.stringify(user_list));
+  };
+
+  // edit user
+  const handleEdit = (index) => {
+    localStorage.setItem("editIndex", index);
+    navigate("/editUser/" + index);
+  };
+
+  // useEffect
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container mt-5">
+      <div>
+        <button
+          className="btn btn-outline-success"
+          onClick={() => navigate("/adduser")}
+        >
+          Add User
+        </button>
+      </div>
+
+      {users.length > 0 ? (
+        <div className="table-responsive mt-3">
+          <table className="table">
+            <thead className="table-primary">
+              <tr>
+                <th>SI.NO</th>
+                <th>Name</th>
+                <th>D.O.B</th>
+                <th>Gender</th>
+                <th>Role</th>
+                <th>Skill</th>
+                <th>Other Skills</th>
+                <th>Address</th>
+                <th>Comments</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((ele, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{ele.name}</td>
+                    <td>{ele.dob}</td>
+                    <td>{ele.gender}</td>
+                    <td>{ele.role}</td>
+                    <td>{`${ele.skill},`}</td>
+                    <td>{ele.otherSkills}</td>
+                    <td>{ele.address}</td>
+                    <td>{ele.comments}</td>
+                    <td className="">
+                      <button
+                        className="btn text-warning"
+                        onClick={() => handleEdit(index)}
+                      >
+                        <MdModeEdit />
+                      </button>
+                      <button
+                        className="btn text-danger"
+                        onClick={() => handleDelete(index)}
+                      >
+                        <AiFillDelete />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="text-center">
+          <h1 className="text-secondary">No Data</h1>
+          <img
+            className="w-50"
+            src="https://cdn.dribbble.com/users/1753953/screenshots/3818675/animasi-emptystate.gif"
+            alt=""
+          />
+        </div>
+      )}
+    </div>
+  );
+}
