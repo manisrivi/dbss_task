ffb87c60acd5817f9656915afb08d916c6b01867
MANIKANDAN S <95971849+manisrivi@users.noreply.github.com>
2023-01-30 13:17:17 +0530

    Update AddUser.js

--- src/AddUser.js
+++ src/AddUser.js
@@ -59,7 +59,7 @@ export default function AddUser() {
                   <h6>
                     D.O.B <span className="text-danger">*</span>
                   </h6>
-                  <Field name="dob" type="date" className="form-control" />
+                  <Field id="date" name="dob" type="date" className="form-control" />
                   {errors.dob && touched.dob ? (
                     <div className="text-danger">{errors.dob}*</div>
                   ) : null}
